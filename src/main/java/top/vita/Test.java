4f715b80ca5b7407b50a36d0766895222f48ffc0
just-vita <1254581982@qq.com>
2023-04-02 15:15:28 +0800

    4.2

--- src/main/java/top/vita/Test.java
+++ src/main/java/top/vita/Test.java
@@ -1,10 +1,13 @@
 package top.vita;
 
+import cn.hutool.core.lang.copier.Copier;
+
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @Author vita
@@ -35,7 +38,24 @@ public class Test {
 //        System.out.println(integer1 == integer2);
 //        integer2.intValue();
 
+//        test();
+
+//        List<Integer> list = Arrays.asList(1, 2, 3);
+//        list.forEach(System.out::println);
+//        System.out.println("list.stream().mapToInt(i -> i).summaryStatistics().getMax() = " + list.stream().mapToInt(i -> i).summaryStatistics().getMax());
+//        Stream<Integer> stream = list.stream();
+//        stream.limit(1);
+//        stream.limit(1);
+//        list.parallelStream().forEach(System.out::println);
+
+
+
 
     }
 
+    public static String test() {
+        String s = "test";
+        s = s.substring(2);
+        return s;
+    }
 }
